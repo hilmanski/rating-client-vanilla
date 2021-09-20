692b6e9960e20007e15d40e2d151dc01a4e73ef5
hilmanski <hilman@hilman.com>
2021-09-20 09:35:57 +0800

    add product page
    add product page with product detail and review

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,16 @@
 const baseURL = 'https://fakerating.deta.dev'
 
+//custom selector
+function $(el) {
+    const type = el.charAt(0)
+    switch (type) {
+        case "#":
+            return document.querySelector(el)
+            break;
+        case ".":
+            return document.querySelectorAll(el)
+            break;
+        default:
+            return document.getElementsByTagName(el)
+    }
+}
